2230d1f7df7da35e9f1aea4d6aa04a58a0698f40
Akash <akashsivanandan@protonmail.com>
2026-01-06 22:50:26 -0500

    Add debug prints in main.go
    Display input string for easier debugging

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"rahu/parser"
 	"rahu/utils"
+	"fmt"
 )
 
 func main() {
-	input := "result = x + y"
+	input := utils.ParseFile("test.py")
+	fmt.Printf("Going to parse input string\n%s\n\n\n", input)
 	p := parser.New(input) // Pass the same input
-
 	module := p.Parse()
 	utils.PrintAST(module, 0)
 }
