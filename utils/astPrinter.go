874dc2339244eae222786c2f6a7682b66f78f1d2
Akash <akashsivanandan@protonmail.com>
2026-01-06 22:50:07 -0500

    feat: extend AST printer for control flow and comparison nodes
    Enhance the AST printer to support newly added parser constructs,
    including
    if/else blocks, for-loops, function definitions, return statements,
    expression
    statements, and tuple-based bodies. Add detailed pretty-printing for
    chained
    comparison expressions and CompareOp variants.
    
    This improves debuggability and makes the parsed AST structure explicit
    and
    readable during development.

--- utils/astPrinter.go
+++ utils/astPrinter.go
@@ -65,6 +65,91 @@ func PrintAST(node any, indent int) {
 			fmt.Printf("%s  Args: []\n", prefix)
 		}
 
+	case *parser.If:
+		fmt.Printf("%sIf:\n", prefix)
+		fmt.Printf("%s  TestCondition:\n", prefix)
+		PrintAST(n.Test, indent + 4)
+		fmt.Printf("%s  Body:\n", prefix)
+		for i := range n.Body{
+			PrintAST(n.Body[i], indent + 4)
+		}
+		if len(n.Orelse) > 0{
+			fmt.Printf("%s  Orelse:\n", prefix)
+			for i := range n.Orelse{
+				PrintAST(n.Orelse[i], indent + 4)
+			}
+		} else {
+			fmt.Printf("%s Orelse: nil\n", prefix)
+		}
+
+	case *parser.ExprStmt:
+		fmt.Printf("%sExprStmt:\n", prefix)
+		PrintAST(n.Value, indent + 2)
+	
+	case string:
+		fmt.Printf("%s%s\n", prefix, n)
+
+	case *parser.FunctionDef:
+		fmt.Printf("%sFunctionDef:\n", prefix)
+		fmt.Printf("%s  FuncName (%s)\n", prefix, n.Name)
+		fmt.Printf("%s  Args:\n", prefix)
+		for i := range n.Args{
+			PrintAST(n.Args[i], indent + 4)
+		}
+
+		fmt.Printf("%s  Body:\n", prefix)
+		for i := range n.Body{
+			PrintAST(n.Body[i], indent + 4)
+		}
+
+	case *parser.Return:
+		fmt.Printf("%sReturn:\n", prefix)
+		PrintAST(n.Value, indent + 2)
+
+	
+	case parser.CompareOp:
+		fmt.Printf("%sCompareOp:\n", prefix)
+		switch n{
+		case parser.Eq:
+			fmt.Printf("%s  Eq()\n", prefix)
+		case parser.Gt:
+			fmt.Printf("%s Gt()\n", prefix)
+		case parser.Lt:
+			fmt.Printf("%s Lt()\n", prefix)
+		case parser.GtE:
+			fmt.Printf("%s GtE()\n", prefix)
+		case parser.LtE:
+			fmt.Printf("%s LtE()\n", prefix)
+		}
+
+	case *parser.Compare:
+		fmt.Printf("%sCompare:\n", prefix)
+		fmt.Printf("%s  Left:\n", prefix)
+
+		PrintAST(n.Left, indent + 4)
+
+		fmt.Printf("%s  Ops:\n", prefix)
+		for i := range n.Ops{
+			PrintAST(n.Ops[i], indent + 4)
+		}
+
+		fmt.Printf("%s  Right:\n", prefix)
+
+		for i := range n.Right{
+			PrintAST(n.Right[i], indent  + 4)
+		}
+
+
+	case *parser.For:
+		fmt.Printf("%sFor:\n", prefix)
+		fmt.Printf("%s  Target:\n", prefix)
+		PrintAST(n.Target, indent + 4)
+		fmt.Printf("%s  Iter:\n", prefix)
+		PrintAST(n.Iter, indent + 4)
+		fmt.Printf("%s  Body:\n", prefix)
+		for i := range n.Body{
+			PrintAST(n.Body[i], indent + 4)
+		}
 	default:
 		fmt.Printf("%sUnknown(%T)\n", prefix, node)
 	}
