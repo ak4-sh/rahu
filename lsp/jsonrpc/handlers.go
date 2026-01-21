5ebacebe811d0aadcc6ce7e3f4b524b98d026b23
Akash <akashsivanandan@protonmail.com>
2026-01-21 13:42:50 -0500

    feat: define a request handler struct
    Expose RegisterRequest and RegisterNotificatoin methods to register new
    handlers

---
+++ lsp/jsonrpc/handlers.go
@@ -0,0 +1,20 @@
+package jsonrpc
+
+import j "encoding/json"
+
+type (
+	RequestHandler      func(j.RawMessage) (j.RawMessage, *Error)
+	NotificationHandler func(j.RawMessage)
+)
+
+var requestHandlers = map[string]RequestHandler{}
+
+var notificationHandlers = map[string]NotificationHandler{}
+
+func RegisterRequest(method string, h RequestHandler) {
+	requestHandlers[method] = h
+}
+
+func RegisterNofication(method string, h NotificationHandler) {
+	notificationHandlers[method] = h
+}
